2d19aed3f43c8053de565b8016b77084c5be6b29
KseniaShkolnikova <naxalu85@gmail.com>
2022-11-29 21:17:43 +0500

    Написали программу, решающую поставленную задачу

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+
+
+string[] LessThanThreeSign(string[] input, int n)  
+{
+    string[] output = new string[GetArray(input, n)];
+
+    for (int i = 0, j = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int GetArray(string[] input, int n)  
+{
+    int count = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] PrintArray()  
+{
+    Console.WriteLine("Введите значения  массива через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
+
+Console.Clear();
+string[] array = PrintArray();
+string[] result = LessThanThreeSign(array, 3);
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
